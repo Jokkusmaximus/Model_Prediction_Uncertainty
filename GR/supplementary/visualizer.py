321f4e72d3b91cc177e3d66095f6c2957829e6e1
Joakim VH <jv.haukedal@tum.de>
2024-05-16 11:52:32 +0200

    small cleanup, and comments

--- GR/supplementary/visualizer.py
+++ GR/supplementary/visualizer.py
@@ -4,6 +4,9 @@ by: jokkus
 '''
 import gymnasium as gym
 import torch
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.buffers import RolloutBuffer
 
 from world_model.model_net import ModelNetwork
 
@@ -16,20 +19,27 @@ obs = torch.tensor(obs)
 obs = obs.type(torch.FloatTensor)
 print(obs.type())
 
-# load model
-model = ModelNetwork.load_from_checkpoint("test_run_1/test/version_0/checkpoints/epoch=24-step=23450.ckpt")
+callback = BaseCallback()
+rollout_buffer = RolloutBuffer()
 
-# eval mode?
-model.eval()
+# load models
+# world_model = ModelNetwork.load_from_checkpoint("logs/model_test_run_1/test/version_0/checkpoints/epoch=24-step=23450.ckpt")
+
+rl_model = PPO.load("logs/rl_test/rl_model_20240508-154616/checkpoints/checkpoint_model_100000_steps.zip")
+rl_model.collect_rollouts(env, callback, rollout_buffer,10)
+
+# eval mode? should set to training mode when using dropout, otherwise the model is run without dropout
 # x = obs + action
 
 # render 1200 times
-for _ in range(1000):
-    # render env
-    env.render()
-    action = model(x)
-    # random action                 # TODO: make AI take action
-    env.step(action)
-    obs, _, _, _, _ = env.step(action)
-    x = torch.concat(obs, action)
-env.close()
+# for _ in range(1000):
+#     # render env
+#     env.render()
+#
+#     obs, _ = env.reset()
+#     action = rl_model(obs)
+#     # random action                 # TODO: make AI take action
+#     env.step(action)
+#     obs, _, _, _, _ = env.step(action)
+#     x = torch.concat(obs, action)
+# env.close()
