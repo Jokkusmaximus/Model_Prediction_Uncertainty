7a51c212126aeb3daaccfb599df042d8dc2a3fd3
Joakim VH <jv.haukedal@tum.de>
2024-06-20 12:12:32 +0200

    set-up for training cleanRL agent with different action_logstd

--- GR/main_model_run.py
+++ GR/main_model_run.py
@@ -7,29 +7,29 @@ import numpy as np
 
 import torch
 import gymnasium as gym
-from lightning.pytorch import seed_everything
+# from lightning.pytorch import seed_everything     # TODO remove
 
 from rl.sb3_agent import train_rl_model
 from supplementary.settings import SEED, PROJECT_ENV
-from supplementary.experiments import ex_different_lr
+from supplementary.experiments import ex_different_lr, ex_different_action_logstd
 
 
 # seeds and generators
-torch.manual_seed(SEED)
-torch.cuda.manual_seed(SEED)
-random.seed(SEED)
-np.random.seed(SEED)
-g = torch.Generator()
-g.manual_seed(SEED)
-seed_everything(SEED, workers=True)
+# torch.manual_seed(SEED)
+# torch.cuda.manual_seed(SEED)
+# random.seed(SEED)
+# np.random.seed(SEED)
+# g = torch.Generator()
+# g.manual_seed(SEED)
+# seed_everything(SEED, workers=True)
+#
+# # create env
+# # NOTE setting up mujoco is troublesome
+# # You can switch to any env you like
+# env = gym.make(PROJECT_ENV)
+# env.action_space.seed(SEED)
 
-# create env
-# NOTE setting up mujoco is troublesome
-# You can switch to any env you like
-env = gym.make(PROJECT_ENV)
-env.action_space.seed(SEED)
-
-ex_different_lr(7, env)
+ex_different_action_logstd()
 
 # train_rl_model(env=env)
 
